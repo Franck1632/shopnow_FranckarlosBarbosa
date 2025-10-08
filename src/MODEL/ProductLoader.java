0c4325a2747a27d02cf8d2384e65ff7eaa446e59
Franckarlos Barbosa <franckarlos.barbosa@uptc.edu.co>
2025-10-08 09:39:38 -0500

    Excepciones basicas

--- src/MODEL/ProductLoader.java
+++ src/MODEL/ProductLoader.java
@@ -1,5 +1,7 @@
 package MODEL;
+
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.ArrayList;
 import java.io.FileReader;
@@ -9,17 +11,23 @@ public class ProductLoader {
     public static List<Product> loadProducts(String pathFile) {
         List<Product> products = new ArrayList<>();
 
-        try (BufferedReader bufferReader = new BufferedReader(new FileReader(pathFile))){
+        try (BufferedReader bufferReader = new BufferedReader(new FileReader(pathFile))) {
             String line;
             while ((line = bufferReader.readLine()) != null) {
                 String[] values = line.split(",");
                 int id = Integer.parseInt(values[0]);
                 String name = values[1];
-                double cost =  Double.parseDouble(values[2]);
+                double cost = Double.parseDouble(values[2]);
                 products.add(new Product(name, cost, id));
             }
+        } catch (FileNotFoundException e) {
+            System.out.println("No se encontro el archivo: " + pathFile);
         } catch (IOException e) {
-            System.out.println("Error leyendo archivo: " + e.getMessage());
+            System.out.println("Error al leer el archivo.");
+        }catch (Exception e){
+            System.out.println("Error General");
+        }finally {
+            System.out.println("Cierra el programa.");
         }
         return products;
     }
